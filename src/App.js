e18f8b8ad60756f04496e5f7029b897933867247
Omarweb <ohissan@hotmail.comb>
2022-06-20 05:50:33 +0200

    remove old code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
-import { useState } from "react";
+
 import Listbooks from "./components/Listbooks";
 import Search from "./components/Search";
 import { Route, Routes } from "react-router";
 
 function App() {
-  const [showSearchPage, setShowSearchpage] = useState(false);
+
 
   return (
     <div className="app">
       <Routes>
-        <Route exact path="/" element={<Listbooks showSearchPage={showSearchPage} setShowSearchpage={setShowSearchpage} />}></Route>
-        <Route exact path="/search" element={<Search showSearchPage={showSearchPage} setShowSearchpage={setShowSearchpage} />}></Route>
+        <Route exact path="/" element={<Listbooks />}></Route>
+        <Route exact path="/search" element={<Search />}></Route>
 
 
 
